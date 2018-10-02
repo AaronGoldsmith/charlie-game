e13188a114db9608e650cdb2987225d9d15f0e73
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2018-10-01 19:41:52 -0700

    UI changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Header = ({children}) => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-s">
-    <a className="navbar-brand" href="/">
-     {children}
-    </a>
-  </nav>
+const Header = (props) => (
+  <div>
+    <nav className="nav navbar navbar-lg bg-primary ">
+        <div className="float-left">
+           <h1>{props.message}</h1>
+        </div>
+        <div className="float-right">
+           <h2>Score:  {props.score}</h2>
+           <h2>High Score:  {props.highscore}</h2>
+        </div>
+      </nav>
+  </div>
 );
 
 export default Header;
